d36a69a8a1b96266aef1d3489487d66daab3e773
Muhamed Omar Makhlouf <md.3omer@gmail.com>
2025-04-25 10:51:01 +0800

    Initial commit

---
+++ Array.java
@@ -0,0 +1,187 @@
+public class Array {
+
+ public static void main ( String []arg ){
+
+    int [] numbers= {1,2,3,4,5};
+
+    // access using index 
+
+    System.out.println("Elemenet of numbers array at index 0:"+ numbers[0]);
+    System.out.println("Elemenet of numbers array at index 0:"+ numbers[0]);
+
+    // modifying elemnts in the array 
+
+
+    numbers[1]= 10; 
+    System.out.println("modification of element at index 1 :"+ numbers[1]);
+
+
+    // iterating 
+
+    System.out.println("Printing all elemnets of the array:");
+    int index1= 0;
+     for (int num : numbers) {
+    System.out.println("Element at index " + index1+ ": " + num);
+    index1++;
+}
+
+    int [] uinitalizedArray= new int [3];
+
+    uinitalizedArray[0]=100;
+    uinitalizedArray[1]=200;
+    uinitalizedArray[2]=300; 
+
+
+
+    int[] initalizedArray= new int [uinitalizedArray.length];
+
+    int index=0; 
+
+    for(int element : uinitalizedArray){
+        initalizedArray[index] = element; 
+        index++; 
+    }
+
+    System.out.println("Prinitng initialized array using enhance for loop : ");
+
+    for( int element : initalizedArray){
+
+        System.out.println("elemnt:" + element);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Cinema.java
@@ -0,0 +1,89 @@
+public class Cinema {
+    private String name;
+    private Seat[][] seats;
+
+    // Constructor to initialize the cinema with given name, rows, and columns
+    public Cinema(String name, int rows, int cols) {
+        this.name = name;
+        this.seats = new Seat[rows][cols];
+        // Initialize seats
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                // Create a new Seat object for each seat in the cinema
+                seats[i][j] = new Seat();
+            }
+        }
+    }
+
+    // Method to reserve a seat at given row and column
+    public void reserveSeat(int row, int col) {
+        // Mark the seat as reserved
+        seats[row - 1][col - 1].setReserved(true);
+    }
+
+    // Method to print the seating plan of the cinema
+    public void printSeats() {
+        System.out.println("Seating Plan for " + name + ":");
+        // Iterate over each row of seats
+        for (Seat[] row : seats) {
+            // Iterate over each seat in the row
+            for (Seat seat : row) {
+                // Print the seat status (reserved or available)
+                System.out.print(seat + " ");
+            }
+            // Move to the next line after printing each row
+            System.out.println();
+        }
+    }
+
+    // Method to get the number of rows in the cinema
+    public int getRowCount() {
+        return seats.length;
+    }
+
+    // Method to get the number of columns in the cinema
+    public int getColumnCount() {
+        // Assuming all rows have the same number of columns
+        return seats[0].length;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        // Creating two cinema halls
+        Cinema cinema1 = new Cinema("Cinema Hall 1", 5, 7);
+        Cinema cinema2 = new Cinema("Cinema Hall 2", 4, 6);
+
+        // Reserving some seats
+        cinema1.reserveSeat(3, 4);
+        cinema1.reserveSeat(1, 2);
+        cinema2.reserveSeat(2, 3);
+        cinema2.reserveSeat(4, 5);
+
+        // Printing seating plan for each cinema hall
+        cinema1.printSeats();
+        System.out.println();
+        cinema2.printSeats();
+
+        // Getting the number of rows and columns for each cinema hall
+        System.out.println("Number of rows in Cinema 1: " + cinema1.getRowCount());
+        System.out.println("Number of columns in Cinema 1: " + cinema1.getColumnCount());
+        System.out.println("Number of rows in Cinema 2: " + cinema2.getRowCount());
+        System.out.println("Number of columns in Cinema 2: " + cinema2.getColumnCount());
+    }
+}
+
+// Class to represent a seat in the cinema
+class Seat {
+    private boolean reserved;
+
+    // Method to set the seat as reserved
+    public void setReserved(boolean reserved) {
+        this.reserved = reserved;
+    }
+
+    // Method to represent the seat as a string (X for reserved, - for available)
+    @Override
+    public String toString() {
+        return reserved ? "X" : "-";
+    }
+}
